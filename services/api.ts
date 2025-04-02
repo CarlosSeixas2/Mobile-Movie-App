198ec7d12a4fd41da69551465ff1b384dbfb4617
Seixas <carlosseixasof@gmail.com>
2025-04-02 12:02:44 -0300

    listando filmes na home

---
+++ services/api.ts
@@ -0,0 +1,31 @@
+export const TMDB_CONFIG = {
+  base_url: "https://api.themoviedb.org/3",
+  api_key: process.env.EXPO_PUBLIC_TMDB_API_KEY,
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${process.env.EXPO_PUBLIC_TMDB_API_KEY}`,
+  },
+};
+
+export const fetchPopularMovies = async ({ query }: { query?: string }) => {
+  if (!TMDB_CONFIG.headers.Authorization) {
+    throw new Error("API Key or Bearer Token is missing");
+  }
+
+  const endpoint = query
+    ? `${TMDB_CONFIG.base_url}/search/movie?query=${encodeURIComponent(query)}`
+    : `${TMDB_CONFIG.base_url}/discover/movie?sort_by=popularity.desc`;
+
+  const response = await fetch(endpoint, {
+    method: "GET",
+    headers: TMDB_CONFIG.headers,
+  });
+
+  if (!response.ok) {
+    // @ts-ignore
+    throw new Error("Failed to fetch movies", response.statusText);
+  }
+
+  const data = await response.json();
+  return data.results;
+};
